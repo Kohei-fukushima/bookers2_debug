95996190f56182ffa482e4146faaa332fd89bd5d
KOHEI FUKUSHIMA <pecpec1129@gmail.com>
2022-05-26 09:06:57 +0000

    [Finish]bookers2-debug

--- Rails6-bookers2-debug-main/app/controllers/books_controller.rb
+++ Rails6-bookers2-debug-main/app/controllers/books_controller.rb
@@ -1,21 +1,27 @@
 class BooksController < ApplicationController
-
+before_action :check_user, only: [:edit, :update]
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
+    @newbook = Book.new
   end
 
   def index
     @books = Book.all
+    @book = Book.new
+    @user = current_user
+
   end
 
   def create
+    @user = current_user
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
       @books = Book.all
-      render 'index'
+      render :index
     end
   end
 
@@ -28,19 +34,27 @@ class BooksController < ApplicationController
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
-      render "edit"
+      render :edit
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title,:body,:profile_image)
   end
+
+  def check_user
+   @book = Book.find(params[:id])
+   unless @book.user == current_user
+   redirect_to books_path
+   end
+  end
+
 end
--- Rails6-bookers2-debug-main/app/controllers/users_controller.rb
+++ Rails6-bookers2-debug-main/app/controllers/users_controller.rb
@@ -10,16 +10,29 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @book = Book.new
+    @user = current_user
+    @books = Book.all
+
   end
 
   def edit
+    @user = User.find(params[:id])
+    if @user ==current_user
+      render :edit
+    else
+      redirect_to user_path(current_user.id)
+    end
+
+
   end
 
   def update
+    @usr = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to user_path(@user.id), notice: "You have updated user successfully."
     else
-      render "show"
+      @books = Book.all
+      render :edit
     end
   end
 
